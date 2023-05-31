3d375dfb07cb054ec8da4e8d97700604c9685ed6
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-01 11:28:34 +1200

    add a temp server

---
+++ server/index.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const server = express()
+
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
