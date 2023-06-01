e21ba87e6e22fd2e66c1662e11ed4ad47c6ce3eb
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-01 16:31:31 +1200

    suppress lint error

--- server/index.ts
+++ server/index.ts
@@ -5,5 +5,6 @@ const server = express()
 const port = process.env.PORT || 3000
 
 server.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server listening on port ${port}`)
 })
