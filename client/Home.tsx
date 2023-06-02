bcb4ed24bf2ddd57c2fcdd5fd62e9e57844b9c48
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-02 15:16:06 +1200

    display access token

--- client/Home.tsx
+++ client/Home.tsx
@@ -1,4 +1,17 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import { useEffect } from 'react'
+
 function Home() {
+  const { isAuthenticated, getAccessTokenSilently } = useAuth0()
+
+  useEffect(() => {
+    async function getAccessToken() {
+      if (!isAuthenticated) return
+      const token = await getAccessTokenSilently()
+      console.log(token)
+    }
+    getAccessToken()
+  }, [isAuthenticated])
   return <div>Home</div>
 }
 
