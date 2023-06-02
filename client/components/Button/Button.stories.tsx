69ad7b0942795545261d2215c90dbadca9e382a8
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-03 09:30:45 +1200

    add a storybook for button component

---
+++ client/components/Button/Button.stories.tsx
@@ -0,0 +1,27 @@
+// Button.stories.ts|tsx
+
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Button from './Button'
+
+const meta: Meta<typeof Button> = {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Primary Button',
+  component: Button,
+}
+
+export default meta
+type Story = StoryObj<typeof Button>
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/react/api/csf
+ * to learn how to use render functions.
+ */
+export const MyPrimary: Story = {
+  name: 'Primary',
+  render: () => <Button>Primary</Button>,
+}
