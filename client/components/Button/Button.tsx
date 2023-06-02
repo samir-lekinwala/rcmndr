bb6026bf3559a8f4ff31c9ceb731455039f607fc
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-03 11:36:53 +1200

    style button

--- client/components/Button/Button.tsx
+++ client/components/Button/Button.tsx
@@ -1,14 +1,14 @@
+import React from 'react'
+
 interface Props {
   children: React.ReactNode
 }
 
 function Button({ children }: Props) {
   return (
-    <div className="bg-purple-600">
-      <button className="bg-blue-500 hover:bg-blue-700 text-pink-50 font-bold py-2 px-4 rounded-full">
-        {children}
-      </button>
-    </div>
+    <button className="w-full hover:font-semibold bg-primary text-white py-2 px-4 rounded-lg">
+      {children}
+    </button>
   )
 }
 
