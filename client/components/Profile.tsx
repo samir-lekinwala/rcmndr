43e25bdec11bed71d38be360c8b708183940d43c
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-02 15:15:27 +1200

    submit form

--- client/components/Profile.tsx
+++ client/components/Profile.tsx
@@ -1,7 +1,10 @@
 import { useAuth0 } from '@auth0/auth0-react'
+import Form from './From'
+import { ProfileDraft } from '../../models/Profile'
 
 function Profile() {
-  const { user, isAuthenticated, isLoading } = useAuth0()
+  const { user, isAuthenticated, isLoading, getAccessTokenSilently } =
+    useAuth0()
 
   console.log({ user, isAuthenticated, isLoading })
   if (isLoading) {
@@ -12,9 +15,26 @@ function Profile() {
     return <div>Not authenticated</div>
   }
 
+  async function handleSubmit(form: ProfileDraft) {
+    const token = await getAccessTokenSilently()
+    const response = await fetch('/api/v1/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(form),
+    })
+
+    if (response.ok) {
+      console.log('success')
+    }
+  }
+
   return (
     <div>
       <p>{user?.email}</p>
+      <Form handleSubmit={handleSubmit} />
     </div>
   )
 }
