5120df4244d24527f0d3738ab9e5aa7669a175b7
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-03 21:04:30 +1200

    fix typo

--- server/db/migrations/20230603035335_songs.js
+++ server/db/migrations/20230603035335_songs.js
@@ -1,7 +1,7 @@
 exports.up = async function(knex) {
   await knex.schema.createTable('songs', (table) => {
     table.increments('id').primary()
-    table.string('user_id').references('users.id').notNullable()
+    table.string('user_id').references('users.auth0_id').notNullable()
     table.string('title').notNullable()
     table.string('artist').notNullable()
     table.string('genre')
