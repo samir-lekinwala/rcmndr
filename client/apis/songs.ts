7615eb69fdcfe7dcaa67f15e9a4eaf787ded4b47
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-03 17:00:13 +1200

    post a song

--- client/apis/songs.ts
+++ client/apis/songs.ts
@@ -7,3 +7,10 @@ export async function getSongs() {
   console.log(response.body)
   return response.body
 }
+
+export async function addSong(id: number, token: string) {
+  await request
+    .post(`${baseUrl}`)
+    .set('Authorization', `Bearer ${token}`)
+    .send({ id })
+}
