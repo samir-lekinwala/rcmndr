7ecb1399c10cd1d02460266cadfd90091ffa2792
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-03 17:47:01 +1200

    add a component for Title

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -4,27 +4,31 @@ import LoginButton from './Login'
 
 function Nav() {
   return (
-    <nav>
-      <ul className="text-3xl">
-        <li>
-          <Link to="profile">My tracks</Link>
-        </li>
-        <li>
-          <Link to="my-tracks">My friends</Link>
-        </li>
-        <li>
-          <Link to="edit-profile">Edit my profile</Link>
-        </li>
-        <li>
-          <Link to="show-qr">Share QR code</Link>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-        <li>
-          <LoginButton />
-        </li>
-      </ul>
+    <nav className="pl-4 pt-3 flex justify-between items-center">
+      <h1 className="text-3xl font-bold text-primary">rcmndr.</h1>
+      <div className="pr-4">
+        <i className="fa-solid fa-bars text-lg "></i>
+      </div>
+      {/* <ul className="text-3xl"> */}
+      {/*   <li> */}
+      {/*     <Link to="profile">My tracks</Link> */}
+      {/*   </li> */}
+      {/*   <li> */}
+      {/*     <Link to="my-tracks">My friends</Link> */}
+      {/*   </li> */}
+      {/*   <li> */}
+      {/*     <Link to="edit-profile">Edit my profile</Link> */}
+      {/*   </li> */}
+      {/*   <li> */}
+      {/*     <Link to="show-qr">Share QR code</Link> */}
+      {/*   </li> */}
+      {/*   <li> */}
+      {/*     <LogoutButton /> */}
+      {/*   </li> */}
+      {/*   <li> */}
+      {/*     <LoginButton /> */}
+      {/*   </li> */}
+      {/* </ul> */}
     </nav>
   )
 }
---
+++ client/components/Title.tsx
@@ -0,0 +1,12 @@
+function Title() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-primary">rcmndr.</h1>
+      <div className="pr-4">
+        <i className="fa-solid fa-bars text-lg "></i>
+      </div>
+    </>
+  )
+}
+
+export default Title
