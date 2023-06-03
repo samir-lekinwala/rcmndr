71c54b36eef9cd41e246b3fa93a64f744e654090
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-03 16:53:56 +1200

    add a route

--- server/routes/songs.ts
+++ server/routes/songs.ts
@@ -3,8 +3,19 @@ import { validateAccessToken } from '../auth0'
 
 const router = express.Router()
 
-router.get('/', validateAccessToken, (req, res) => {
-  res.send('songs')
+// GET /api/v1/songs/
+router.get('/', (req, res) => {
+  const songs = [
+    {
+      id: 1,
+      title: 'Song 1',
+      artist: 'Artist 1',
+      link: 'link 1',
+      genre: 'genre 1',
+    },
+  ]
+
+  res.json(songs)
 })
 
 export default router
