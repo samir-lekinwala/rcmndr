d38178fdfa367c24566d146df3695b571cf03e77
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-03 17:58:00 +1200

    comment out nav

--- client/components/AppLayout.tsx
+++ client/components/AppLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Nav from './Nav'
+import Title from './Title'
 
 function AppLayout() {
   return (
     <div className="bg-darkPurple h-full text-white">
-      <Nav />
+      {/* <Nav /> */}
+      <Title />
       <Outlet />
     </div>
   )
