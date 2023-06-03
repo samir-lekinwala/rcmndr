8437c03cb5523ca4ce14ab629cf997836a91f999
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-03 14:19:04 +1200

    add MyTracks Page

---
+++ client/Pages/MyTracks.tsx
@@ -0,0 +1,11 @@
+function MyTracks() {
+  return (
+    <div>
+      <h1>My Tracks</h1>
+      <h4>These are the tracks you have recommended</h4>
+      <ul></ul>
+    </div>
+  )
+}
+
+export default MyTracks
