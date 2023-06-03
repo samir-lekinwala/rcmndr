b64df0477b33fbdc12a00686bb2528f6a96ea0c8
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-03 21:06:15 +1200

    improve styling

--- client/Pages/MyTracks.tsx
+++ client/Pages/MyTracks.tsx
@@ -16,10 +16,10 @@ function MyTracks() {
   })
 
   return (
-    <div className="pl-4 pr-4">
+    <div className="pl-4 pr-4 mt-8">
       <h1 className="text-3xl">Brenegade</h1>
       <h2 className="text-xs">These are the tracks you have recommended</h2>
-      <ul className="flex flex-col gap-4 mt-8">
+      <ul className="flex flex-col gap-4 mt-6">
         {!isLoading &&
           data &&
           data.map((track) => (
