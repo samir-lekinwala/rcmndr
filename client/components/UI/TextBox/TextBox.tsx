dc5806b8fd14caba99b3179c5873f453fe6d24f1
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-03 13:31:10 +1200

    fix props

--- client/components/UI/TextBox/TextBox.tsx
+++ client/components/UI/TextBox/TextBox.tsx
@@ -1,15 +1,12 @@
 import { InputHTMLAttributes } from 'react'
 
-interface Props {
-  defaultValue?: string
-}
-
 type InputAttribues = InputHTMLAttributes<HTMLInputElement>
 
-function TextBox({ defaultValue, ...rest }: InputAttribues) {
+function TextBox({ ...rest }: InputAttribues) {
   return (
     <input
-      className="w-full bg-[#301453] placeholder-[#B07CF2] text-white px-3 py-2 text-sm leading-tight border shadow appearance-none focus:outline-none focus:shadow-outline rounded-lg"
+      type="text"
+      className="px-3 py-2 bg-[#301453] text-white border focus:shadow-[0px_0px_9px_2px_#B07CF2] border-transparent placeholder-[#B07CF2] disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none block w-full rounded-md sm:text-sm invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:shadow-none"
       {...rest}
     />
   )
