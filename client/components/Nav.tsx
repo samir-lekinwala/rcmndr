fd731f159db8129276d003e04c207fce89f8c291
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-03 17:47:08 +1200

    refactor

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -5,30 +5,26 @@ import LoginButton from './Login'
 function Nav() {
   return (
     <nav className="pl-4 pt-3 flex justify-between items-center">
-      <h1 className="text-3xl font-bold text-primary">rcmndr.</h1>
-      <div className="pr-4">
-        <i className="fa-solid fa-bars text-lg "></i>
-      </div>
-      {/* <ul className="text-3xl"> */}
-      {/*   <li> */}
-      {/*     <Link to="profile">My tracks</Link> */}
-      {/*   </li> */}
-      {/*   <li> */}
-      {/*     <Link to="my-tracks">My friends</Link> */}
-      {/*   </li> */}
-      {/*   <li> */}
-      {/*     <Link to="edit-profile">Edit my profile</Link> */}
-      {/*   </li> */}
-      {/*   <li> */}
-      {/*     <Link to="show-qr">Share QR code</Link> */}
-      {/*   </li> */}
-      {/*   <li> */}
-      {/*     <LogoutButton /> */}
-      {/*   </li> */}
-      {/*   <li> */}
-      {/*     <LoginButton /> */}
-      {/*   </li> */}
-      {/* </ul> */}
+      <ul className="text-3xl">
+        <li>
+          <Link to="profile">My tracks</Link>
+        </li>
+        <li>
+          <Link to="my-tracks">My friends</Link>
+        </li>
+        <li>
+          <Link to="edit-profile">Edit my profile</Link>
+        </li>
+        <li>
+          <Link to="show-qr">Share QR code</Link>
+        </li>
+        <li>
+          <LogoutButton />
+        </li>
+        <li>
+          <LoginButton />
+        </li>
+      </ul>
     </nav>
   )
 }
