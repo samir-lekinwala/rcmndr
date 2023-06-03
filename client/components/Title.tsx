58aa629a6a9db9cadaf8dbab92a014177c31c764
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-03 17:57:50 +1200

    improve styling

--- client/components/Title.tsx
+++ client/components/Title.tsx
@@ -1,11 +1,11 @@
 function Title() {
   return (
-    <>
+    <div className="pl-4 pt-3 flex justify-between items-center">
       <h1 className="text-3xl font-bold text-primary">rcmndr.</h1>
       <div className="pr-4">
         <i className="fa-solid fa-bars text-lg "></i>
       </div>
-    </>
+    </div>
   )
 }
 
