74f1f72dbc9b1a15e35332f69581158444c9c769
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-03 13:30:54 +1200

    wrap textbox with background

--- client/components/UI/TextBox/TextBox.stories.tsx
+++ client/components/UI/TextBox/TextBox.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 import TextBox from './TextBox'
+import Background from '../Background/Background'
 
 const meta: Meta<typeof TextBox> = {
   title: 'Text Box',
@@ -12,7 +13,12 @@ type Story = StoryObj<typeof TextBox>
 export const MyPrimary: Story = {
   name: 'Input',
   render: () => (
-    <TextBox defaultValue="sdfsdfsdf sd fsdf" placeholder="Enter here" />
+    <Background>
+      <TextBox
+        defaultValue="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
+        placeholder="Enter here"
+      />
+    </Background>
   ),
 }
 
