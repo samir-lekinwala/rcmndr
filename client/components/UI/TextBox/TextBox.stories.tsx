038ee0b8b76b21225cfe6cd2b2559e2896825b63
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-03 13:39:29 +1200

    add more stories

--- client/components/UI/TextBox/TextBox.stories.tsx
+++ client/components/UI/TextBox/TextBox.stories.tsx
@@ -10,14 +10,37 @@ const meta: Meta<typeof TextBox> = {
 
 type Story = StoryObj<typeof TextBox>
 
-export const MyPrimary: Story = {
-  name: 'Single',
+export const populated: Story = {
+  name: 'Populated text box',
   render: () => (
     <Background>
       <TextBox
         defaultValue="Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat."
         placeholder="Enter here"
       />
+    </Background>
+  ),
+}
+
+export const emptyTextBox: Story = {
+  name: 'Empty text box',
+  render: () => (
+    <Background>
+      <TextBox placeholder="Enter here" />
+    </Background>
+  ),
+}
+
+export const multipleTextBoxes: Story = {
+  name: 'Multiple text boxes',
+  render: () => (
+    <Background>
+      <div className="space-y-2">
+        <TextBox placeholder="The full title of the song" />
+        <TextBox placeholder="Name of the artist / singer / group" />
+        <TextBox placeholder="Genre of music (optional)" />
+        <TextBox placeholder="A link so others can listen (optional)" />
+      </div>
     </Background>
   ),
 }
