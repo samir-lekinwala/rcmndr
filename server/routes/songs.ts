434be81285dbe1eca028c3bac78a0dbeb73d1252
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-03 21:05:52 +1200

    implement routes

--- server/routes/songs.ts
+++ server/routes/songs.ts
@@ -1,27 +1,45 @@
 import express from 'express'
+
+import * as db from '../db/songs'
 import { validateAccessToken } from '../auth0'
+import { song, songDraftSchema } from '../../types/Song'
 
 const router = express.Router()
 
 // GET /api/v1/songs/
-router.get('/', validateAccessToken, (req, res) => {
-  const songs = [
-    {
-      id: 1,
-      title: 'Song 1',
-      artist: 'Artist 1',
-      link: 'link 1',
-      genre: 'genre 1',
-    },
-  ]
-
-  res.json(songs)
+router.get('/', validateAccessToken, async (req, res) => {
+  try {
+    const userId = req.auth?.payload.sub
+    if (!userId) {
+      return res.status(401).json({ error: 'unauthorized' })
+    }
+    const songs = await db.getSongs(userId)
+    res.json(songs)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'unable to retrieve songs from database' })
+  }
 })
 
 // POST /api/v1/songs/
-router.post('/', validateAccessToken, (req, res) => {
-  console.log(req.body)
-  res.sendStatus(200)
+router.post('/', validateAccessToken, async (req, res) => {
+  try {
+    const userId = req.auth?.payload.sub
+    if (!userId) {
+      return res.status(401).json({ error: 'unauthorized' })
+    }
+    const song = {
+      user_id: userId,
+      ...songDraftSchema.parse(req.body),
+    }
+    console.log(song)
+
+    await db.insertSong(song)
+    res.sendStatus(201)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'unable to insert song into database' })
+  }
 })
 
 export default router
