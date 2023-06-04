09170715d0e25fde158040a4c296a3972072315d
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-04 18:00:52 +1200

    add link on logo

--- client/components/Logo/Logo.tsx
+++ client/components/Logo/Logo.tsx
@@ -1,5 +1,11 @@
+import { Link } from 'react-router-dom'
+
 function Title() {
-  return <h1 className="text-3xl font-bold text-primary">rcmndr.</h1>
+  return (
+    <Link to="/">
+      <h1 className="text-3xl font-bold text-primary">rcmndr.</h1>
+    </Link>
+  )
 }
 
 export default Title
