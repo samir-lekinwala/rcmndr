d50e9b89c7cf1c175f85039094cc9e005339ea4e
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-04 18:05:53 +1200

    turn off autocompletion

--- client/components/UI/TextBox/TextBox.tsx
+++ client/components/UI/TextBox/TextBox.tsx
@@ -7,6 +7,7 @@ function TextBox({ ...rest }: InputAttribues) {
     <input
       type="text"
       className="p-2 bg-[#301453] text-white border focus:shadow-[0px_0px_9px_2px_#B07CF2] border-transparent placeholder-[#B07CF2] focus:outline-none block w-full rounded-md sm:text-sm"
+      autoComplete="off"
       {...rest}
     />
   )
