893496936c68e2fe6a69ee4a6120d35c4e1d7448
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-04 12:11:17 +1200

    remove guard

--- client/components/Login.tsx
+++ client/components/Login.tsx
@@ -1,8 +1,9 @@
 import { useAuth0 } from '@auth0/auth0-react'
+
 import Button from './UI/Button/Button'
 
 function LoginButton() {
-  const { loginWithRedirect, isAuthenticated } = useAuth0()
+  const { loginWithRedirect } = useAuth0()
 
   return <Button onClick={() => loginWithRedirect()}>Log In</Button>
 }
--- client/components/Logout.tsx
+++ client/components/Logout.tsx
@@ -1,9 +1,9 @@
 import { useAuth0 } from '@auth0/auth0-react'
 
 function LogoutButton() {
-  const { logout, isAuthenticated } = useAuth0()
+  const { logout } = useAuth0()
 
-  return isAuthenticated ? (
+  return (
     <button
       onClick={() =>
         logout({ logoutParams: { returnTo: window.location.origin } })
@@ -11,7 +11,7 @@ function LogoutButton() {
     >
       Log Out
     </button>
-  ) : null
+  )
 }
 
 export default LogoutButton
