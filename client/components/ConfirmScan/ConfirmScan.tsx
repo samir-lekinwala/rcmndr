7118013284493505e501ddd32e9732caf33482e4
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-05 19:11:20 +1200

    improve UX

--- client/components/ConfirmScan/ConfirmScan.tsx
+++ client/components/ConfirmScan/ConfirmScan.tsx
@@ -9,9 +9,16 @@ function ConfirmScan(props: Props) {
 
   return (
     <div className="flex flex-col pl-4 h-screen items-center justify-center space-y-4">
+      <p>Scan is successful</p>
       <p>
-        Would you like to follow <b>{props.name}</b>
+        Would you like to follow
+        <strong className="bg-primary transform w-1/2  skew-x-12">
+          {' '}
+          {props.name}{' '}
+        </strong>
+        ?
       </p>
+
       <div className="space-x-4">
         <Button onClick={handleFollow}>Follow</Button>
         <Button className="bg-white text-primary">Cancel</Button>
