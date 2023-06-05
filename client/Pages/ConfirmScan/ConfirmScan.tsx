da9bd68e3ab7fa73a624d6f236a83cef5e7d4223
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-05 19:28:34 +1200

    display consumed api

--- client/Pages/ConfirmScan/ConfirmScan.tsx
+++ client/Pages/ConfirmScan/ConfirmScan.tsx
@@ -2,17 +2,27 @@ import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 import ConfirmScan from '../../components/ConfirmScan/ConfirmScan'
+import { useQuery } from 'react-query'
+import { getUser } from '../../apis/user'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function Code() {
+  const { getAccessTokenSilently } = useAuth0()
   const { code } = useParams()
 
-  useEffect(() => {
-    console.log(code)
-  }, [code])
+  const { data, isLoading, isError, error } = useQuery(
+    ['getUser'],
+    async () => {
+      const token = await getAccessTokenSilently()
+      if (code && token) {
+        return await getUser(code, token)
+      }
+    }
+  )
 
   async function handleFollow() {}
 
-  return <ConfirmScan name={'banana'} />
+  return data && <ConfirmScan name={data?.nickname} />
 }
 
 export default Code
