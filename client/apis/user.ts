756cd177c86dc2e0330fc157bbb3c444a0e2c581
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-05 19:45:19 +1200

    consume follow route

--- client/apis/user.ts
+++ client/apis/user.ts
@@ -8,3 +8,12 @@ export async function getUser(auth0: string, token: string) {
 
   return res.body
 }
+
+export async function followUser(auth0: string, token: string) {
+  const res = await request
+    .post(`/api/v1/users/${auth0}/follow`)
+    .set('Authorization', `Bearer ${token}`)
+    .set('Content-Type', 'application/json')
+
+  return res.body
+}
