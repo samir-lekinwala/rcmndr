153ed9104ffa05e712b1cfe250a69cda3e999f71
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-05 19:29:57 +1200

    fix import order

--- client/Pages/ConfirmScan/ConfirmScan.tsx
+++ client/Pages/ConfirmScan/ConfirmScan.tsx
@@ -1,10 +1,9 @@
-import { useEffect } from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
 import { useParams } from 'react-router-dom'
+import { useQuery } from 'react-query'
 
 import ConfirmScan from '../../components/ConfirmScan/ConfirmScan'
-import { useQuery } from 'react-query'
 import { getUser } from '../../apis/user'
-import { useAuth0 } from '@auth0/auth0-react'
 
 function Code() {
   const { getAccessTokenSilently } = useAuth0()
@@ -22,7 +21,13 @@ function Code() {
 
   async function handleFollow() {}
 
-  return data && <ConfirmScan name={data?.nickname} />
+  return (
+    <>
+      {data && <ConfirmScan name={data?.nickname} />}
+      {isLoading && <p>loading</p>}
+      {isError && <p>Unable to load information</p>}
+    </>
+  )
 }
 
 export default Code
