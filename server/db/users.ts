e6e0222f4efa29ec1db9e70c14ede2eb5b44368a
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-06 17:36:58 +1200

    fix relationship to meet schema in migrations

--- server/db/users.ts
+++ server/db/users.ts
@@ -2,8 +2,8 @@ import db from './connection'
 import { Friend } from '../../types/User'
 
 export async function getFriends(userId: string) {
-  return (await db('followers')
+  return (await db('following_list')
     .join('users', 'users.auth0_id', 'followers.following_id')
     .select('users.auth0_id', 'nickname', 'first_name as firstName')
-    .where('follower_id', userId)) as Friend[]
+    .where('user_id', userId)) as Friend[]
 }
