8d758c38ae0f4b970e43b7161836afd4acdacb18
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-06 17:00:40 +1200

    update seed

--- server/db/seeds/2-followers.js
+++ server/db/seeds/2-followers.js
@@ -1,7 +1,13 @@
 exports.seed = async function (knex) {
   await knex('followers').insert([
-    { follower_id: 'auth0|103', following_id: 'auth0|102' },
-    { follower_id: 'auth0|103', following_id: 'auth0|104' },
+    {
+      follower_id: 'auth0|6478f3fd75374ee3d7bc4d94',
+      following_id: 'auth0|102',
+    },
+    {
+      follower_id: 'auth0|6478f3fd75374ee3d7bc4d94',
+      following_id: 'auth0|104',
+    },
     { follower_id: 'auth0|101', following_id: 'auth0|102' },
     { follower_id: 'auth0|101', following_id: 'auth0|103' },
     { follower_id: 'auth0|101', following_id: 'auth0|104' },
