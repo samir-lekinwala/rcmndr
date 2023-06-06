266782f958de420f9fcf68ba6fca066b1e8f82fd
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-06 17:01:16 +1200

    fix getFriends by using a join

--- server/db/users.ts
+++ server/db/users.ts
@@ -2,7 +2,8 @@ import db from './connection'
 import { Friend } from '../../types/User'
 
 export async function getFriends(userId: string) {
-  return (await db('users')
-    .select('id', 'nickname')
+  return (await db('followers')
+    .join('users', 'users.auth0_id', 'followers.following_id')
+    .select('users.auth0_id', 'nickname', 'first_name as firstName')
     .where('follower_id', userId)) as Friend[]
 }
