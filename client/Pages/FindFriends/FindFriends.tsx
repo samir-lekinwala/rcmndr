8b01a83effd098cce993881398e7edce56febb40
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-07 08:29:46 +1200

    consume api to display suggestions

--- client/Pages/FindFriends/FindFriends.tsx
+++ client/Pages/FindFriends/FindFriends.tsx
@@ -1,7 +1,25 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import { useState } from 'react'
+
 import MyFriends from '../MyFriends/MyFriends'
 import TextBox from '../../components/UI/TextBox/TextBox'
+import { searchFriends } from '../../apis/user'
+import { Friend } from '../../../types/User'
 
 function FindFriends() {
+  const { getAccessTokenSilently } = useAuth0()
+  const [searchQuery, setSearchQuery] = useState('')
+  const [suggestedFriends, setSuggestedFriends] = useState([] as Friend[])
+
+  async function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearchQuery(() => e.target.value.trim())
+    const token = await getAccessTokenSilently()
+    const suggestions = await searchFriends(e.target.value.trim(), token)
+    console.log(suggestions)
+
+    setSuggestedFriends(() => suggestions)
+  }
+
   return (
     <div className="p-4 mt-10 space-y-10">
       <div className="space-y-2">
@@ -9,9 +27,22 @@ function FindFriends() {
         <h2 className="text-xl font-semibold">Follow a new friend</h2>
         <div className="flex items-baseline gap-4">
           <i className="fa-solid fa-search" />
-          <TextBox placeholder="Search by a genre, nickname or a real name" />
+          <TextBox
+            placeholder="Search by a genre, nickname or a real name"
+            onChange={handleChange}
+            value={searchQuery}
+          />
         </div>
       </div>
+      <ul>
+        {suggestedFriends.map((friend) => (
+          <li key={friend.id}>
+            <div className="flex items-center gap-4">
+              <p>{friend.nickname}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
       <div>
         <MyFriends />
       </div>
