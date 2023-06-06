6cde6d26512ed3a894e7c7aef4ca91ddba698aae
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-06 17:02:07 +1200

    response with real data from db

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -5,6 +5,27 @@ import { validateAccessToken } from '../auth0'
 
 const router = express.Router()
 
+// GET /api/v1/users/friends
+router.get('/friends', validateAccessToken, async (req, res) => {
+  const id = req.auth?.payload.sub
+
+  if (!id) {
+    res.status(400).json({ message: 'Please provide an id' })
+    return
+  }
+
+  try {
+    const friends = await db.getFriends(id)
+    res.status(200).json(friends)
+  } catch (e) {
+    console.error(e)
+    if (e instanceof Error) {
+      res.status(500).json({ message: e.message })
+    }
+  }
+})
+
+// GET /api/v1/users/:id
 router.get('/:id', validateAccessToken, (req, res) => {
   const id = req.params.id
 
@@ -15,31 +36,14 @@ router.get('/:id', validateAccessToken, (req, res) => {
   res.status(200).json({ nickname: 'test' })
 })
 
-router.get('/friends', validateAccessToken, (req, res) => {
-  const id = req.auth?.payload.sub
-
-  if (!id) {
-    res.status(400).json({ message: 'Please provide an id' })
-    return
-  }
-
-  try {
-    const friends = db.getFriends(id)
-    res.status(200).json(friends)
-  } catch (e) {
-    console.error(e)
-    if (e instanceof Error) {
-      res.status(500).json({ message: e.message })
-    }
-  }
-})
-
+// POST /api/v1/users
 router.post('/', validateAccessToken, (req, res) => {
   const form = req.body
 
   res.status(200).json({ message: form })
 })
 
+// POST /api/v1/users/:userId/follow
 router.post('/:userId/follow', validateAccessToken, async (req, res) => {
   const userId = req.params.userId
 
