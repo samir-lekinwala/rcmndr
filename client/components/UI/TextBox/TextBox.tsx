bd4ec0998b8f8a334a278daaba5fb5cf0ab82bc6
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-06 17:54:40 +1200

    merge classNames to extend styles

--- client/components/UI/TextBox/TextBox.tsx
+++ client/components/UI/TextBox/TextBox.tsx
@@ -1,12 +1,16 @@
 import { InputHTMLAttributes } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 type InputAttribues = InputHTMLAttributes<HTMLInputElement>
 
-function TextBox({ ...rest }: InputAttribues) {
+function TextBox({ className, ...rest }: InputAttribues) {
   return (
     <input
       type="text"
-      className="p-2 bg-[#301453] text-white border focus:shadow-[0px_0px_9px_2px_#B07CF2] border-transparent placeholder-[#B07CF2] focus:outline-none block w-full rounded-md sm:text-sm"
+      className={twMerge(
+        'p-2 bg-[#301453] text-white border focus:shadow-[0px_0px_9px_2px_#B07CF2] border-transparent placeholder-[#B07CF2] focus:outline-none block w-full rounded-md sm:text-sm',
+        className
+      )}
       autoComplete="off"
       {...rest}
     />
