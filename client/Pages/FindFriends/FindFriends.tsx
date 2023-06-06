ec6de4dd5584f0c0c9c66f4b6e94e10ffe111955
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-06 17:55:45 +1200

    show UI components for find-friends route

--- client/Pages/FindFriends/FindFriends.tsx
+++ client/Pages/FindFriends/FindFriends.tsx
@@ -1,7 +1,17 @@
+import MyFriends from '../MyFriends/MyFriends'
+import TextBox from '../../components/UI/TextBox/TextBox'
+
 function FindFriends() {
   return (
-    <div>
-      <h1>Find Friends</h1>
+    <div className="p-4 mt-10 space-y-10">
+      <div>
+        <h1 className="text-3xl font-semibold">My friends</h1>
+        <h2>Follow a new friend</h2>
+        <TextBox placeholder="Search by a genre, nickname or a real name" />
+      </div>
+      <div>
+        <MyFriends />
+      </div>
     </div>
   )
 }
