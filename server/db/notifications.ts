4c902b1dfa4ae2cc2edf3809de9971f637984d89
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-08 15:42:56 +1200

    filter by unread notifications

--- server/db/notifications.ts
+++ server/db/notifications.ts
@@ -3,5 +3,6 @@ import db from './connection'
 export async function getNotifications(userId: string) {
   return db('notifications')
     .where('user_id', userId)
+    .andWhere('is_read', false)
     .orderBy('timestamp', 'desc')
 }
