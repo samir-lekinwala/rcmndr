4657096703b96d3cbcc7d30a56e491f44f085a07
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-08 15:22:35 +1200

    add a new table for notifications

---
+++ server/db/migrations/20230608021029_notifications.js
@@ -0,0 +1,13 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('notifications', (table) => {
+    table.increments('id').primary()
+    table.string('user_id').references('users.auth0_id')
+    table.string('friend_id').references('users.auth0_id')
+    table.boolean('is_read').defaultTo(false) // automatically set to false
+    table.integer('timestamp')
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('notifications')
+}
