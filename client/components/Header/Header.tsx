4e83295a135962740f7c522bee00a92da223c52e
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-08 17:20:36 +1200

    update padding

--- client/components/Header/Header.tsx
+++ client/components/Header/Header.tsx
@@ -11,7 +11,7 @@ function Header() {
   }
 
   return (
-    <div className="pl-4 pt-3 flex justify-between items-center">
+    <div className="pl-4 pt-3 pr-4 flex justify-between items-center">
       <Logo />
       {!navOpened && (
         <button onClick={toggleMenu}>
