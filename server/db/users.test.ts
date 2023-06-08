d32710edcab5134a740ea84fd293608d8660ea94
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-08 13:55:00 +1200

    add a test file

---
+++ server/db/users.test.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect, beforeAll, beforeEach } from 'vitest'
+import db from './connection'
+import { searchFriends } from './users'
+
+beforeAll(async () => {
+  await db.migrate.latest()
+})
+
+beforeEach(async () => {
+  await db.seed.run()
+})
+
+describe('getFriends', () => {
+  it('added friends should not be suggested', async () => {
+    const friends = await searchFriends('auth0|6478f3fd75374ee3d7bc4d94', 'rem')
+    expect(friends).toHaveLength(0)
+  })
+
+  it('non added friends should be suggested', async () => {
+    const friends = await searchFriends('auth0|6478f3fd75374ee3d7bc4d94', 'boi')
+
+    console.log(friends)
+    expect(friends).toHaveLength(1)
+  })
+})
