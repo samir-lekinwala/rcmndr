4ca100476f45ba3abf5d54f9f9d94031799bb3ac
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-08 16:46:28 +1200

    fix order of deletion based on relationships

--- server/db/seeds/0-cleanup.js
+++ server/db/seeds/0-cleanup.js
@@ -1,6 +1,6 @@
 exports.seed = async function (knex) {
-  await knex('songs').del()
   await knex('notifications').del()
+  await knex('songs').del()
   await knex('following_list').del()
   await knex('users').del()
 }
