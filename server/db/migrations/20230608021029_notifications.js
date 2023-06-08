3c5c15931a69ceaaa4934fea4a5beed6b81d9b6c
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-08 16:47:51 +1200

    add comments for clarity

--- server/db/migrations/20230608021029_notifications.js
+++ server/db/migrations/20230608021029_notifications.js
@@ -1,11 +1,11 @@
 exports.up = function (knex) {
   return knex.schema.createTable('notifications', (table) => {
     table.increments('id').primary()
-    table.string('user_id').references('users.auth0_id')
-    table.string('friend_id').references('users.auth0_id')
-    table.string('song_id').references('songs.id')
+    table.string('user_id').references('users.auth0_id') // the user who is receiving the notification
+    table.string('friend_id').references('users.auth0_id') // the user who is sending the notification
+    table.string('song_id').references('songs.id') // the song that is being shared
     table.boolean('is_read').defaultTo(false) // automatically set to false
-    table.integer('timestamp')
+    table.integer('timestamp') // the time the notification was sent
   })
 }
 
