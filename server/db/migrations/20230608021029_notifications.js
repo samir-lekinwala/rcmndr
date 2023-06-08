63368e6eac5521f24c9f1ec125eb49eede07f318
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-08 16:46:52 +1200

    add a relationship to notifications

--- server/db/migrations/20230608021029_notifications.js
+++ server/db/migrations/20230608021029_notifications.js
@@ -3,6 +3,7 @@ exports.up = function (knex) {
     table.increments('id').primary()
     table.string('user_id').references('users.auth0_id')
     table.string('friend_id').references('users.auth0_id')
+    table.string('song_id').references('songs.id')
     table.boolean('is_read').defaultTo(false) // automatically set to false
     table.integer('timestamp')
   })
