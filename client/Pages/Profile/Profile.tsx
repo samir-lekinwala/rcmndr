2b81c3b75c6dfe1511ae4e404abb0ae6b3552c6a
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-11 08:18:14 +1200

    rename component

--- client/Pages/Profile/Profile.tsx
+++ client/Pages/Profile/Profile.tsx
@@ -1,7 +1,7 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import { useNavigate } from 'react-router-dom'
 
-import Form from '../../components/Form/Form'
+import Form from '../../components/ProfileForm/ProfileForm'
 import { ProfileDraft } from '../../../types/Profile'
 
 function Profile() {
