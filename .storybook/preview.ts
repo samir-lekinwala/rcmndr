bd2d87d582da29e8166de2e5a612d48b06f5a516
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-15 17:23:58 +1200

    import fontawesome so that fonts are viewable in storybooks

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,7 @@ import type { Preview } from '@storybook/react'
 // import the generated tailwind css file so that
 // we can use tailwind classes in our stories
 import '../public/main.css'
+import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/js/all.min.js'
 
 const preview: Preview = {
   parameters: {
