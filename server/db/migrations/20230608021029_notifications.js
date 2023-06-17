f9e6f8396234a75b7ede5fbe74f0240b3bd36bb6
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-17 15:32:37 +1200

    add message as a column to notifications table

--- server/db/migrations/20230608021029_notifications.js
+++ server/db/migrations/20230608021029_notifications.js
@@ -1,14 +1,14 @@
-exports.up = function (knex) {
+exports.up = function(knex) {
   return knex.schema.createTable('notifications', (table) => {
     table.increments('id').primary()
     table.string('user_id').references('users.auth0_id') // the user who is receiving the notification
     table.string('friend_id').references('users.auth0_id') // the user who is sending the notification
-    table.string('song_id').references('songs.id') // the song that is being shared
+    table.string('message')
     table.boolean('is_read').defaultTo(false) // automatically set to false
     table.integer('timestamp') // the time the notification was sent
   })
 }
 
-exports.down = function (knex) {
+exports.down = function(knex) {
   return knex.schema.dropTable('notifications')
 }
--- server/db/notifications.ts
+++ server/db/notifications.ts
@@ -3,16 +3,14 @@ import db from './connection'
 export async function getNotifications(userId: string) {
   return db('notifications')
     .join('users', 'notifications.friend_id', 'users.auth0_id')
-    .join('songs', 'notifications.song_id', 'songs.id')
     .where('notifications.user_id', userId)
     .andWhere('is_read', false)
     .orderBy('timestamp', 'desc')
     .select(
       'notifications.id as notificationId',
       'notifications.timestamp as notificationTimestamp',
-      'users.nickname',
-      'songs.title as songTitle',
-      'songs.genre as songGenre'
+      'notifications.message',
+      'users.nickname'
     )
 }
 
