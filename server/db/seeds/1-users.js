6d4135bf6651c79398a8618fb76aaad8a8586bf9
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-06-20 07:51:38 +1200

    update seed file

--- server/db/seeds/1-users.js
+++ server/db/seeds/1-users.js
@@ -1,14 +1,16 @@
-exports.seed = async function (knex) {
+exports.seed = async functionknex) {
   await knex('users').insert([
     {
       auth0_id: 'auth0|6478f3fd75374ee3d7bc4d94',
+      email: 'jared@example.com',
       first_name: 'Jared',
       last_name: 'Pinfold',
       nickname: 'DestroyOrbison',
       public: true,
     },
     {
-      auth0_id: 'auth0|648fd33d73375442becf2c84',
+      auth0_id: 'auth0|648fd1c873375442becf2c60',
+      email: 'remington@example.com',
       first_name: 'Remington',
       last_name: 'Smythe',
       nickname: 'Remmy',
@@ -16,6 +18,7 @@ exports.seed = async function (knex) {
     },
     {
       auth0_id: 'auth0|649024f773375442becf3102',
+      email: 'gertrude@example.com',
       first_name: 'Gertrude',
       last_name: 'Diamond',
       nickname: 'D1am0nd',
@@ -23,6 +26,7 @@ exports.seed = async function (knex) {
     },
     {
       auth0_id: 'auth0|6490255b0c2119ef3db1e4aa',
+      email: 'sloane@example.com',
       first_name: 'Sloane',
       last_name: 'Trousers',
       nickname: 'STrousers',
@@ -30,6 +34,7 @@ exports.seed = async function (knex) {
     },
     {
       auth0_id: 'auth0|649025b96ef896963ad50b3f',
+      email: 'steve@example.com',
       first_name: 'Steve',
       last_name: 'Puce',
       nickname: 'SecretBoi2001',
