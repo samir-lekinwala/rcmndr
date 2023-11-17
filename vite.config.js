2283e5c6ba22f08ddeb9701d8205d75420fdf85e
Daphne Simons <daphne.simons@devacademy.co.nz>
2023-11-17 15:25:56 +1300

    2 current problems - on build getting two manifest files generated AND there's a syntax error in the registerSW.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,ts,tsx,css,html,ico,png,svg}'],
+      },
+      manifest: {
+        name: 'rcmndr',
+        short_name: 'rcmndr',
+        start_url: '/',
+        orientation: 'portrait',
+        display: 'standalone',
+        background_color: '#FF17CE',
+        theme_color: '#1B0636',
+        icons: [
+          {
+            src: '/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
       '/api': {
