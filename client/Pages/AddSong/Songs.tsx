ad0a42a7dead90c79034634752c77b0828d03de6
Kirsty Ammundsen <kirstyammundsen@hotmail.com>
2023-11-23 14:44:30 +1300

    Add client files for addsong

---
+++ client/Pages/AddSong/Songs.tsx
@@ -0,0 +1,10 @@
+function Songs() {
+  return (
+    <div>
+      <h1>Add new song</h1>
+      <h2>Fill in the details below to add a new song to your list</h2>
+    </div>
+  )
+}
+
+export default Songs
