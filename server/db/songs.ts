90f6fbfc2f50bab65d599a7d5a5daa0d09a2c378
David <14233215+davidlpoole@users.noreply.github.com>
2023-11-23 15:36:11 +1300

    Hide banned songs

--- server/db/songs.ts
+++ server/db/songs.ts
@@ -1,5 +1,16 @@
 import db from './connection'
 
 export async function getSongs() {
-  return await db('songs').select()
+  return await db('songs')
+    .select(
+      'id',
+      'user_id',
+      'title',
+      'artist',
+      'genre',
+      'link',
+      'comments',
+      'is_banned'
+    )
+    .where('is_banned', false)
 }
