ea3ecb99acbbb8291b6f55b810bafa0b353cf508
Logan Bruce <l.t.bruce6739@gmail.com>
2023-11-23 13:05:58 +1300

    feature: added reports table

---
+++ server/db/migrations/20231122223838_reports.js
@@ -0,0 +1,15 @@
+export async function up(knex) {
+  await knex.schema.createTable('reports', (table) => {
+    table.string('id').primary()
+    table.timestamp('created_on')
+    table.string('reported_by')
+    table.integer('reason_id')
+    table.boolean('is_processed')
+    table.integer('song_id')
+    table.integer('moderator_id')
+  })
+}
+
+export async function down(knex) {
+  await knex.schema.dropTable('reports')
+}
