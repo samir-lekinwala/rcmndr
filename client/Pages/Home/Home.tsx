f44595a75641ab29cad7517fcc30c91acbff00f2
Ricky <65267289+ricky-huang-dev@users.noreply.github.com>
2023-11-24 10:14:02 +1300

    individually stagger slogan words

--- client/Pages/Home/Home.tsx
+++ client/Pages/Home/Home.tsx
@@ -1,73 +1,27 @@
-import { useEffect } from 'react'
 import LoginButton from '../../components/Login/Login'
 import RegisterButton from '../../components/RegisterButton/RegisterButton'
-import { useAnimation, motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 
 function Home() {
-  // const [scope, animate] = useAnimate()
-
-  // useEffect(() => {
-  //   animate(
-  //     scope.current,
-  //     { opacity: 1 },
-  //     { delay: stagger(0.1, { from: 'first' }) }
-  //   )
-  // })
-  //   return (
-  //     <div className="pt-44 pl-4 flex flex-col gap-4">
-  //       <div>
-  //         <p className="text-4xl">collate.</p>
-  //         <p className="text-4xl">recommend.</p>
-  //         <p className="text-4xl">discover.</p>
-  //       </div>
-  //       <div className="flex gap-2">
-  //         <LoginButton />
-  //         <RegisterButton />
-  //       </div>
-  //     </div>
-  //   )
-  // }
-  const controls = useAnimation()
-  useEffect(() => {
-    const staggerAnimation = async () => {
-      await controls.start((i) => ({
-        opacity: 1,
-        transition: { duration: 5 },
-        delay: i * 0.2,
-      }))
-    }
-    staggerAnimation()
-  }, [controls])
+  const slogan = ['collate.', 'recommend.', 'discover.']
   return (
-    <div className="pt-44 pl-4 flex flex-col gap-4">
-      <div>
-        <motion.p
-          initial={{ opacity: 0 }}
-          animate={controls}
-          custom={0}
-          style={{ marginBottom: `10px` }}
-          className="text-4xl"
-        >
-          collate.
-        </motion.p>
-        <motion.p
-          initial={{ opacity: 0 }}
-          animate={controls}
-          custom={1}
-          style={{ marginBottom: `10px` }}
-          className="text-4xl"
-        >
-          recommend.
-        </motion.p>
-        <motion.p
-          initial={{ opacity: 0 }}
-          animate={controls}
-          custom={2}
-          style={{ marginBottom: `10px` }}
-          className="text-4xl"
-        >
-          discover.
-        </motion.p>
+    <div>
+      <div
+        className="pt-44 pl-4 flex flex-col gap-4"
+        style={{ marginBottom: `40px` }}
+      >
+        {slogan.map((word, i) => (
+          <div key={word}>
+            <motion.p
+              initial={{ opacity: 1, translateX: -250 }}
+              animate={{ opacity: 1, translateX: 0 }}
+              transition={{ duration: 0.7, delay: i * 1 }}
+              className="text-4xl"
+            >
+              {word}
+            </motion.p>
+          </div>
+        ))}
       </div>
       <div className="flex gap-2">
         <LoginButton />
