ce1456012008e9c869e27f1f9cc1f2e0766f9309
Xavier <143090232+xavier-kirkpatrick@users.noreply.github.com>
2023-11-24 15:14:04 +1300

    show data on my songs page

--- client/Pages/MySongs/MySongs.tsx
+++ client/Pages/MySongs/MySongs.tsx
@@ -1,7 +1,29 @@
+import { Song } from '../../../types/Song'
+import SongListItem from '../../components/SongListItem/SongListItem'
+
 function MySongs() {
+  const fakeTrack: Song = {
+    id: '1',
+    title: 'Hengelo',
+    artist: 'Spring Offensive',
+    genre: 'Indie Rock',
+    link: 'https://open.spotify.com/track/4rqpg85XNApASjAvqjHlb1?si=2bdc00343f3e47f2',
+  }
+
+  function handleEditSong() {}
+
+  function handleDeleteSong() {}
+
   return (
     <div>
       <h1>My Songs</h1>
+      <div>
+        <SongListItem
+          handleEditSong={handleEditSong}
+          handleDeleteSong={handleDeleteSong}
+          song={fakeTrack}
+        />
+      </div>
     </div>
   )
 }
