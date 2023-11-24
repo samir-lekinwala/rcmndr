5a2006978e4b6d78273995b486c40da83b58d470
David <14233215+davidlpoole@users.noreply.github.com>
2023-11-24 16:14:39 +1300

    Display songs from API

--- client/Pages/MySongs/MySongs.tsx
+++ client/Pages/MySongs/MySongs.tsx
@@ -1,13 +1,39 @@
+import { useQuery } from '@tanstack/react-query'
 import { Song } from '../../../types/Song'
 import SongListItem from '../../components/SongListItem/SongListItem'
+import { getSongs } from '../../apis/songs'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function MySongs() {
-  const fakeTrack: Song = {
-    id: '1',
-    title: 'Hengelo',
-    artist: 'Spring Offensive',
-    genre: 'Indie Rock',
-    link: 'https://open.spotify.com/track/4rqpg85XNApASjAvqjHlb1?si=2bdc00343f3e47f2',
+  // const data: Song[] = [
+  //   {
+  //     id: '1',
+  //     title: 'Hengelo',
+  //     artist: 'Spring Offensive',
+  //     genre: 'Indie Rock',
+  //     link: 'https://open.spotify.com/track/4rqpg85XNApASjAvqjHlb1?si=2bdc00343f3e47f2',
+  //   },
+  // ]
+
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0()
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['mySongs'],
+    queryFn: () => getAccessTokenSilently().then((token) => getSongs(token)),
+  })
+
+  console.log(data)
+
+  if (!isAuthenticated && !user) {
+    return <div>Not authenticated</div>
+  }
+
+  if (isLoading) {
+    return <p>loading...</p>
+  }
+
+  if (isError) {
+    return <p>something went wrong</p>
   }
 
   function handleEditSong() {}
@@ -18,11 +44,17 @@ function MySongs() {
     <div>
       <h1>My Songs</h1>
       <div>
-        <SongListItem
-          handleEditSong={handleEditSong}
-          handleDeleteSong={handleDeleteSong}
-          song={fakeTrack}
-        />
+        {data &&
+          data.map((track) => {
+            return (
+              <SongListItem
+                key={track.id}
+                handleEditSong={handleEditSong}
+                handleDeleteSong={handleDeleteSong}
+                song={track}
+              />
+            )
+          })}
       </div>
     </div>
   )
