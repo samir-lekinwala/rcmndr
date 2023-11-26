bff691096318492baee317c3db5f9bec38f7663a
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-11-27 07:40:34 +1300

    remove unnecessary deletion

--- server/db/seeds/4-reasons.js
+++ server/db/seeds/4-reasons.js
@@ -1,15 +1,9 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 export async function seed(knex) {
-  // Deletes ALL existing entries
-  await knex('reasons').del()
   await knex('reasons').insert([
-    { reason: 'Sexual-Harrasment' },
-    { reason: 'Harrasment' },
-    { reason: 'Violence' },
-    { reason: 'Discrimination' },
-    { reason: 'Racism' },
+    {reason: 'Sexual-Harrasment'},
+    {reason: 'Harrasment'},
+    {reason: 'Violence'},
+    {reason: 'Discrimination'},
+    {reason: 'Racism'},
   ])
 }
