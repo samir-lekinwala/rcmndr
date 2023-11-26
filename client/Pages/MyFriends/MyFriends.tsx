a78fa6669f3418a19464d451af3420a551815f5b
Jared Pinfold <j.pinfold@gmail.com>
2023-11-27 12:18:06 +1300

    connect friends list to API/backend

--- client/Pages/MyFriends/MyFriends.tsx
+++ client/Pages/MyFriends/MyFriends.tsx
@@ -1,18 +1,50 @@
+import { useQuery } from '@tanstack/react-query'
+import { getFriends } from '../../apis/user'
 import useUpdateTitle from '../../hooks/useUpdateTitle'
+import { useAuth0 } from '@auth0/auth0-react'
+import { FriendsListItem } from '../../components/FriendsListItem/FriendsListItem'
 
 function MyFriends() {
   useUpdateTitle('MyFriends')
-  const data = [{ id: 1, nickname: 'First user' }]
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0()
 
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['myFriends'],
+    queryFn: async () => {
+      const token = await getAccessTokenSilently()
+      const songs = await getFriends(token)
+      return songs
+    },
+  })
+
+  if (!isAuthenticated && !user) {
+    return <div>Not authenticated</div>
+  }
+
+  if (isLoading) {
+    return <p>loading...</p>
+  }
+
+  if (isError) {
+    return <p>something went wrong</p>
+  }
+  
   return (
-    <div className="space-y-4">
-      <h1 className="text-xl font-semibold">My existing friends</h1>
-      <ul className="space-y-4">
+    <div>
+      {/* Todo: add search functionality */}
+      <h1>My friends</h1>
+      <div>
         {data &&
-          data?.map((friend) => <li key={friend.id}>{friend.nickname}</li>)}
-      </ul>
+          data.map((friend) => {
+            return (
+              <FriendsListItem key={friend.id}/>
+            )
+          })}
+      </div>
     </div>
   )
 }
 
 export default MyFriends
+
+
