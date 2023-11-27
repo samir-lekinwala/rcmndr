9824ca120bcfb3c5f6c5c333a7260089f8d5ffd7
Kirsty Ammundsen <kirstyammundsen@hotmail.com>
2023-11-27 14:18:02 +1300

    add spotify UI

---
+++ client/components/UI/Spotify/Spotify.tsx
@@ -0,0 +1,15 @@
+function Spotify() {
+  return (
+    <iframe
+      title="Here In Spirit"
+      className="p-4"
+      src="https://open.spotify.com/embed/track/6ICdz2wvVMDC4u801OwHA2?utm_source=generator"
+      width="100%"
+      height="152"
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+    ></iframe>
+  )
+}
+
+export default Spotify
