45981a724e0193f7084b4d1ab81dfe1b79170d42
Jared Pinfold <j.pinfold@gmail.com>
2023-11-28 10:41:02 +1300

    implemented friend songs in FriendList

--- client/apis/songs.ts
+++ client/apis/songs.ts
@@ -11,3 +11,11 @@ export async function getSongs(token: string): Promise<Song[]> {
 
   return response.body as Song[]
 }
+
+export async function getFriendSongs(friendId: string, token: string): Promise<Song[]> {
+  const response = await request
+    .get(`${baseUrl}friend/${friendId}`)
+    .set('Authorization', `Bearer ${token}`)
+
+  return response.body as Song[]
+}
--- client/components/FriendsListItem/FriendsListItem.tsx
+++ client/components/FriendsListItem/FriendsListItem.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import { Friend } from '../../../types/User'
 import Icon from '../UI/Icon/Icon'
+import FriendSongs from '../FriendSongs/FriendSongs'
 
 interface Props {
   friend: Friend
@@ -7,18 +9,24 @@ interface Props {
 }
 
 function FriendsListItem(props: Props) {
+  const [expanded, setExpanded] = useState(false)
   const { friend, handleDeleteFriend } = props
-  const { nickname, firstName, lastName,  id } = friend
+  const { nickname, firstName, lastName, id } = friend
   return (
     <li className="list-none flex gap-4">
       <div className="self-center flex-none">
-        <Icon>
-          <i className="fa-solid fa-user text-black" title="friend" />
-        </Icon>
+        <button onClick={() => setExpanded((state) => !state)}>
+          <Icon>
+            <i className="fa-solid fa-user text-black" title="friend" />
+          </Icon>
+        </button>
       </div>
       <div className="flex flex-col w-36 flex-auto">
         <h3 className="text-white">{nickname}</h3>
         <h4 className="text-xs text-lightPurple">{`${firstName} ${lastName}`}</h4>
+        {expanded &&
+        <FriendSongs friend={friend}/>
+      }
       </div>
       <div className="flex flex-row gap-2 self-center flex-none">
         <button onClick={() => handleDeleteFriend(id)}>
@@ -27,6 +35,7 @@ function FriendsListItem(props: Props) {
           </Icon>
         </button>
       </div>
+      
     </li>
   )
 }
